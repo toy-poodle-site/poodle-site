918d29b2cc85ff3acd44c4d940b75cc9107214a2
MarkyJD <54533821+MarkyJD@users.noreply.github.com>
2023-01-13 10:00:30 +1100

    fix: cleaned errors

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,5 +5,5 @@ type TitleProps = {
 };
 
 export default function Title({ children }: TitleProps) {
-  return <h1>{children}</h1>;
+  return <h1 className="text-blue-800 text-3xl">{children}</h1>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Layout from '@/components/Layout';
 import Title from '@/components/Title';
-import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <Layout>
-      <Hero image="" tagline="" description="" ctaLink="" />
       <Title>Hello TypeScript!</Title>
     </Layout>
   );
